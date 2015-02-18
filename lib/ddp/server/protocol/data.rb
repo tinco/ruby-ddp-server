c23145fb1337c9fef6ecd2e7be98883d271ecdba
Tinco Andringa <mail@tinco.nl>
2015-02-18 01:58:24 +0100

    make the interface slightly nicer

--- lib/ddp/server/protocol/data.rb
+++ lib/ddp/server/protocol/data.rb
@@ -6,58 +6,58 @@ module DDP
 				def handle_data
 					case @message['msg']
 					when 'sub'
-						handle_sub
+						handle_sub(@message['id'], @message['name'], @message['params'])
 						true
 					when 'unsub'
-						handle_unsub
+						handle_unsub(@message['id'])
 						true
 					else
 						false
 					end
 				end
 
-				def handle_sub
+				def handle_sub(id, name, params)
 					raise 'Must be overridden'
 				end
 
-				def handle_unsub
+				def handle_unsub(id)
 					raise 'Must be overridden'
 				end
 
-				def nosub(id, error = nil)
+				def send_nosub(id, error = nil)
 					message = { msg: 'nosub', id: id }
 					message.merge!(error: error) if error
 					write_message message
 				end
 
-				def added(collection, id, fields = nil)
+				def send_added(collection, id, fields = nil)
 					message = { msg: 'added', id: id, collection: collection }
 					message.merge!(fields: fields) if fields
 					write_message message
 				end
 
-				def changed(collection, id, fields = nil, cleared = nil)
+				def send_changed(collection, id, fields = nil, cleared = nil)
 					message = { msg: 'changed', id: id, collection: collection }
 					message.merge!(fields: fields) if fields
 					message.merge!(cleared: cleared) if cleared
 					write_message message
 				end
 
-				def removed(collection, id)
+				def send_removed(collection, id)
 					write_message msg: 'removed', collection: collection, id: id
 				end
 
-				def ready(subs)
+				def send_ready(subs)
 					write_message msg: 'ready', subs: subs
 				end
 
-				def added_before(collection, id, fields = nil, before = nil)
+				def send_added_before(collection, id, fields = nil, before = nil)
 					message = { msg: 'addedBefore', id: id, collection: collection, before: before }
 					message.merge!(fields: fields) if fields
 					write_message message
 				end
 
-				def moved_before(collection, id, before = nil)
+				def send_moved_before(collection, id, before = nil)
 					write_message msg: 'movedBefore', id: id, collection: collection, before: before
 				end
 			end
--- lib/ddp/server/protocol/rpc.rb
+++ lib/ddp/server/protocol/rpc.rb
@@ -3,33 +3,39 @@ module DDP
 		module Protocol
 			# Protocol regarding remote procedure calls
 			module RPC
+				NO_RESULT = Object.new
+
 				def handle_rpc
 					case @message['msg']
 					when 'method'
-						handle_method
+						handle_method(@message['id'], @message['method'], @message['params'])
 						true
 					else
 						false
 					end
 				end
 
-				def handle_method
+				def handle_method(id, method, params)
 					raise 'Must be overridden'
 				end
 
-				def result(id, result = nil)
+				def send_result(id, result = NO_RESULT)
 					message = { msg: 'result', id: id }
-					if result
-						if result['error']
-							message['error'] = result
-						else
-							message['result'] = result
-						end
-					end
+					message['result'] = result unless result == NO_RESULT
 					write_message(message)
 				end
 
-				def updated(methods)
+				def send_error_result(id, error)
+					message = { msg: 'result', id: id }
+					message['error'] = { 
+						error: error.class.name,
+						reason: error.message,
+						details: "Backtrace: \n#{error.backtrace.join("\n")}"
+					}
+					write_message(message)
+				end
+
+				def send_updated(methods)
 					write_message msg: 'updated', methods: methods
 				end
 			end
